2c782cfc20dd694b0ee6d98ee7b44d694e90851a
aakimov <aakimov@playtox.ru>
2023-11-25 16:31:21 +0700

    season 4: lecture 1: quickSort

---
+++ yandex_lectures/season4/lecture1/taskB/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var a []int
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanWords)
+
+	scanner.Scan()
+	n, _ := strconv.Atoi(scanner.Text())
+	//fmt.Println(n)
+
+	for i := 0; i < n; i++ {
+		scanner.Scan()
+		ai, _ := strconv.Atoi(scanner.Text())
+		a = append(a, ai)
+	}
+	//fmt.Println(a)
+
+	qSort(0, n)
+	fmt.Print(strings.Trim(fmt.Sprint(a), "[]"))
+}
+
+func qSort(l, r int) {
+	len := r - l
+	if len == 0 || len == 1 {
+		return
+	}
+
+	p := partition(l, r)
+
+	qSort(l, p)
+	qSort(p+1, r)
+}
+
+func partition(l, r int) int {
+	xIdx := rand.Intn(r - l)
+	x := a[xIdx+l]
+
+	equal := l
+	greater := l
+
+	for now := l; now < r; now++ {
+		y := a[now]
+
+		if y < x {
+			a[now] = a[greater]
+			a[greater] = a[equal]
+			a[equal] = y
+
+			equal++
+			greater++
+		}
+
+		if y == x {
+			a[now] = a[greater]
+			a[greater] = y
+
+			greater++
+		}
+	}
+
+	return equal
+}
